e46408e0acaf8028492860998195b8c41db2080c
Praisyz <praisyvarghese2021@gmail.com>
2025-06-13 17:20:35 -0400

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -5,6 +5,8 @@ height = 100
 i = 1
 while( i <= 10):
     newHeight = (3/5)*height
+    # round to 4 digits
+    # print(i, round(newHeight, 4))
     print(i, newHeight)
     height = newHeight
     i = i + 1
